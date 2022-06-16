98d32ddccc9031bd23ec67d7e6908e313d69fd55
mizki <wwr8462@gmail.com>
2022-06-17 01:03:29 +0900

    ブログ完成

---
+++ next/pages/index.js
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/Layout";
+import { getPostsData } from "../lib/post";
+import styles from "../styles/Home.module.css";
+import utilStyle from "../styles/utils.module.css";
+
+
+// SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData(); // id, title, date, thumbnail
+  console.log(allPostsData);
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyle.headingMd}>
+        <p>Next.js勉強中</p>
+      </section>
+      <section>
+        <h2>📝どすこいどすこい</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({ id, title, date, thumbnail }) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img
+                  src={`${thumbnail}`}
+                  className={styles.thumbnailImage}
+                />
+              </Link>
+              <Link href={`/posts/${id}`}>
+                <a className={utilStyle.boldText}>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyle.lightText}>{date}</small>
+            </article>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
+}
